3a44ff6821c616a24662f34292dd4b31c3d44ea2
sherifgerges <sherif_gerges@g.harvard.edu>
2017-01-06 22:22:08 -0500

    Prog. Assign. 2.
    Programming Assignment 2. Compute square matrix inversion.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Sherif G. Jan. 7, 2017. 
+## makeCacheMatrix builds an object containing 4 functions within it. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        #set values of x and m
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        #just return the matrix. 
+        get <- function(){
+                x
+        }
+        setInverse <- function(inverse){
+                i <<- inverse
+        }
+        getInverse <- function(){
+                i
+        }
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+##Computes the inverse of a function, or retrives it if it has already been calculated. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        if(ncol(x$get()) != nrow(x$get())) stop("Not an invertible matrix")
+        i <- x$getInverse()
+        #check to see if matrix inverse has already been computed. 
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setInverse(i)
+        i
 }
+
+
